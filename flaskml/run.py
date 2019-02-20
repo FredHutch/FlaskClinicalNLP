99f046f2d49fa56fc103ea51aeddb8beaf71750f
Scott White <whiteau@fhcrc.org>
2019-02-20 14:09:55 -0800

    Updates to use new version of MedLPInterface

--- flaskml/run.py
+++ flaskml/run.py
@@ -1,4 +1,12 @@
+import argparse
 from flaskml import create_app
 
 app = create_app()
-app.run(host='0.0.0.0')
+
+cmd_arg_parser = argparse.ArgumentParser("")
+cmd_arg_parser.add_argument('-e',  default='0.0.0.0',
+                            dest="endpoint",
+                            help="Set endpoint. Default is 0.0.0.0 ")
+cmd_args = cmd_arg_parser.parse_args()
+
+app.run(host=cmd_args.endpoint)
